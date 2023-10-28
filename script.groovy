f7ab96b291f27f0e945dd7b0050878324927cfdd
MarouaMad <127895493+MarouaMad@users.noreply.github.com>
2023-10-28 09:39:39 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,4 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'gihubToken', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t mimi360/maven-app-demo:1.2 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push mimi360/maven-app-demo:1.2'
-    }
-} 
 
 def deployApp() {
     echo 'deploying the application...'
